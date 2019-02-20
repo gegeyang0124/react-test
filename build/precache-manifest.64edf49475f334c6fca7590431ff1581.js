9ded09f38cc2c43e3967e1ea5b5a5035a76c834b
zy <584450903@qq.com>
2019-02-20 08:40:29 +0800

    Signed-off-by: zy <584450903@qq.com>

---
+++ build/precache-manifest.64edf49475f334c6fca7590431ff1581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "1183353bf34a82858bef",
+    "url": "./static/js/main.1183353b.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "./static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "1183353bf34a82858bef",
+    "url": "./static/css/main.a3682702.chunk.css"
+  },
+  {
+    "revision": "4391318d6078e67000bc73b4cfd2f784",
+    "url": "./index.html"
+  }
+];
